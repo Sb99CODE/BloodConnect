ef96077cd2336de51ff99422a673086aca1182fa
Sujal Bhati <138096045+Sb99CODE@users.noreply.github.com>
2024-09-15 20:31:57 +0530

    Update analyticsRoutes.js

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const authMiddelware = require("../middlewares/authMiddelware");
+const authMiddleware = require("../middlewares/authMiddleware");
 const {
   bloodGroupDetailsContoller,
 } = require("../controllers/analyticsController");
 
 const router = express.Router();
 
-router.get("/bloodGroups-data", authMiddelware, bloodGroupDetailsContoller);
+router.get("/bloodGroups-data", authMiddleware, bloodGroupDetailsContoller);
 
 module.exports = router;
